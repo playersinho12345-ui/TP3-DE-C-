b33046065f1096124da6ce069a78f52be8db9e37
Gabriel Fiorini <playersinho12345@gmail.com>
2026-09-05 15:04:11 -0300

    Add files via upload

---
+++ TP3-C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using TP3_C_.models;
+
+namespace TP3_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro jeep = new Carro();
+            jeep.modelo = "Jeep";
+            jeep.nome = "Jeep Wrangler";
+            jeep.exibirInformacoes();
+        }
+    }
+}
---
+++ TP3-C#/models/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP3_C_.models
+{
+    public class Carro /// Classe criada para criar a entidade que representa "Carro".
+    {
+        public String modelo; /// Ambos são atributos relacionados à classe Carro.
+        public String nome;
+
+        public void exibirInformacoes() /// Método criado para exibir as informações do carro
+        {
+            System.Console.WriteLine("Modelo: " + modelo);
+            System.Console.WriteLine("Nome: " + nome);
+        }
+    }
+}
+
