679498fcc5781d7f0dd34f7458b058f5ba74da10
Gabriel Fiorini <playersinho12345@gmail.com>
2026-09-05 15:02:56 -0300

    Add files via upload

---
+++ exercicio6ao9/TestaMatricula.cs
@@ -0,0 +1,25 @@
+﻿using exercicio6ao9.models;
+
+namespace exercicio6ao9
+{
+    internal class TestaMatricula
+    {
+        static void Main(string[] args)
+        {
+            Matricula pedro1234 = new Matricula();
+
+            pedro1234.NomeDoAluno = "Pedro Alberto";
+            pedro1234.NumeroMatricula = 1;
+            pedro1234.Curso = "Engenharia de Software";
+            pedro1234.Situacao = "Concluída";
+            pedro1234.DataInicial = "13/09/2000";
+
+
+            pedro1234.Trancar();
+            pedro1234.ExibirInformacoes();
+            System.Console.WriteLine("");
+            pedro1234.Reativar();
+            pedro1234.ExibirInformacoes();
+        }
+    }
+}
---
+++ exercicio6ao9/models/Matricula.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio6ao9.models
+{
+    public class Matricula
+    {
+        public String NomeDoAluno;
+        public String Curso;
+        public int NumeroMatricula;
+        public String Situacao; /// Trancado / Ativa / Conluída
+        public String DataInicial; /// (DD/MM/YYYY)
+
+
+        public void Trancar()
+        {
+            this.Situacao = "Trancado";
+        }
+
+        public void Reativar()
+        {
+            this.Situacao = "Ativa";
+        }
+
+        public void ExibirInformacoes()
+        {
+            System.Console.WriteLine(NomeDoAluno);
+            System.Console.WriteLine(Curso);
+            System.Console.WriteLine(NumeroMatricula);
+            System.Console.WriteLine(Situacao);
+            System.Console.WriteLine(DataInicial);
+
+        }
+
+
+
+
+    }
+}
