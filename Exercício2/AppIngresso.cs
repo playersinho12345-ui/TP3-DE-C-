5d6db200534e0f2e666aad8175882ad0bd6cea68
Gabriel Fiorini <playersinho12345@gmail.com>
2026-09-05 15:02:21 -0300

    Add files via upload

---
+++ Exercício2/AppIngresso.cs
@@ -0,0 +1,27 @@
+﻿using Exercício2ao6.models;
+
+namespace Exercício2ao6
+{
+    internal class AppIngresso
+    {
+        static void Main(string[] args)
+        {
+            Ingresso Gorillaz = new Ingresso(
+                "Gorillaz Insane Show",
+                9000.0,
+                300
+                );
+
+            Gorillaz.AlterarQuantidade(30);
+            Gorillaz.AlterarPreco(50);
+
+            Gorillaz.Setpreco(2000);
+            Gorillaz.SetnomeDoshow("Gorillaz");
+            Gorillaz.SetquantidadeDisponivel(500);
+
+            System.Console.WriteLine(Gorillaz.Getnome());
+            System.Console.WriteLine(Gorillaz.Getpreco());
+            System.Console.WriteLine(Gorillaz.GetquantidadeDisponivel());
+        }
+    }
+}
---
+++ Exercício2/models/Ingresso.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício2ao6.models
+{
+    public class Ingresso
+    {
+        public string nomeDoshow; /// Importante para estar ciente sobre qual show está se falando.
+        public double preco; /// Essencial para manipular o preço dos ingressos daquele show.
+        public int quantidadeDisponivel; /// Indispensável para controle do número de ingressos disponíveis.
+
+        public Ingresso(string nomeDoshow, double preco, int quantidadeDisponivel)
+        {
+            this.nomeDoshow = nomeDoshow;
+            this.preco = preco;
+            this.quantidadeDisponivel = quantidadeDisponivel;
+        }
+
+        public void AlterarPreco(double NovoPreco)
+        {
+            this.preco = NovoPreco;
+        }
+
+        public void AlterarQuantidade(int NovaQuantidade)
+        {
+            this.quantidadeDisponivel = NovaQuantidade;
+        }
+
+        public void ExibirInformacoes()
+        {
+            System.Console.WriteLine("Nome do Show: " + nomeDoshow);
+            System.Console.WriteLine("Preço: " + preco);
+            System.Console.WriteLine("Quantidade disponível: " +  quantidadeDisponivel);
+        }
+
+
+        public String Getnome()
+        {
+            return nomeDoshow;
+        }
+
+        public Double Getpreco()
+        {
+            return preco;
+        }
+
+        public int GetquantidadeDisponivel()
+        {
+            return quantidadeDisponivel;
+        }
+
+        public void SetnomeDoshow(string nomeDoshow)
+        {
+            this.nomeDoshow= nomeDoshow;
+        }
+
+        public void Setpreco(double preco)
+        {
+            this.preco = preco;
+        }
+
+        public void SetquantidadeDisponivel( int quantidadeDisponivel)
+        {
+            this.quantidadeDisponivel = quantidadeDisponivel;
+        }
+
+    }
+}
